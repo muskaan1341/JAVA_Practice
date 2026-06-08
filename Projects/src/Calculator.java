1e7893d93d2474f332828c36e6b91547086b369f
Muskaan <muskaan@magureinc.com>
2026-06-08 16:06:39 +0530

    Student_grade.java

--- Projects/src/Calculator.java
+++ Projects/src/Calculator.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main{
+    public static void main( String[] args ) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the first number: ");
