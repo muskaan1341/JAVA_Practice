7e5be5f80cf1b3117acae1edfd2350cde68b32ab
Muskaan <muskaan@magureinc.com>
2026-06-08 16:22:54 +0530

    Student_grade.java

---
+++ Mini_projects_conceptwise/src/personal_information.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class personal_information {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the name of the personal information");
+        System.out.print("Enter the name : ");
+        String name = sc.nextLine();
+        System.out.print("Enter the surname: ");
+        String surname = sc.nextLine();
+        System.out.print("Enter the email: ");
+        String email = sc.nextLine();
+        System.out.print("Enter the phone number: ");
+        String phone = sc.nextLine();
+        System.out.print("Enter the address: ");
+        String address = sc.nextLine();
+
+        System.out.println("Name : " + name);
+        System.out.println("Surname : " + surname);
+        System.out.println("Email : " + email);
+        System.out.println("Phone : " + phone);
+        System.out.println("Address : " + address);
+    }
+}
