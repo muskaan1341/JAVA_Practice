b0b533789b323742c358ea27db0ae15337c9abae
Muskaan <muskaan@magureinc.com>
2026-06-08 16:05:45 +0530

    Student_grade.java

---
+++ Projects/src/Calculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main{
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int b = sc.nextInt();
+
+        System.out.println(a+b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(a-b);
+    }
+}
